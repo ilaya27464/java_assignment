6dc05d696d5475d3754aaa3565a620a1f6b845da
ilaya27464 <127576283+ilaya27464@users.noreply.github.com>
2023-03-25 14:34:58 +0530

    Update Converting.java

--- Converting.java
+++ Converting.java
@@ -1,4 +1,4 @@
-public class Converting {
+public class Conversion {
     public static void main(String[] args) {
 
         int [] arr1 = new int [] {2,7,4,6,4 };
@@ -18,3 +18,14 @@ public class Converting {
         }
     }
 }
+
+OUTPUT:
+
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=12943:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java java\out\production\java java" Conversion
+Elements of original array: 
+2 7 4 6 4 
+Elements of new array: 
+2 7 4 6 4 
+Process finished with exit code 0
+
